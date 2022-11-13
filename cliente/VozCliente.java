6d76cc1872009e38467e63a868a213cccf835046
Camila-Ferr <cf_alves@id.uff.br>
2022-11-13 19:06:25 -0300

    menu

--- cliente/VozCliente.java
+++ cliente/VozCliente.java
@@ -1,16 +1,25 @@
 package cliente;
 
+import java.util.Scanner;
+
 public class VozCliente {
     public static boolean calling = false;
     public static void main(String[] args){
+
+        Scanner teclado = new Scanner(System.in);
         Cliente cliente = new Cliente();
 
         while (true) {
             System.out.println("Digite 1 para iniciar :");
             System.out.println("Digite 2 para parar :");
+            int opcao = teclado.nextInt();
 
-
-            cliente.init_audio();
+            if (opcao == 1) {
+                cliente.init_audio();
+            }
+            else {
+                calling = false;
+            }
 
         }
 
